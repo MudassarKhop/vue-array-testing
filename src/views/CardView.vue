bc65dcff8fc28daefcf5f509a1693a00b0d56902
MudassarKhop <khopatkarmudassar@gmail.com>
2022-07-12 16:16:13 +0200

    update 2

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -1,6 +1,8 @@
 <template>
-	<div class="display">
-		<card v-for="product in products" :key="product.id" :product="product" />
+	<div class="container">
+		<div class="display">
+			<card v-for="product in products" :key="product.id" :product="product" />
+		</div>
 	</div>
 </template>
 <script>
@@ -21,6 +23,21 @@
 	};
 </script>
 <style scoped>
+	template {
+		background-color: black;
+	}
+	.container {
+		height: auto;
+		color: rgb(255, 255, 255);
+		box-sizing: content-box;
+		margin: 0 auto 0 auto;
+		border-radius: 4px;
+		border: 1px solid rgba(255, 255, 255, 0.5);
+		padding: 1rem 1.5rem 2rem 1.5rem;
+		backdrop-filter: blur(50px);
+		box-shadow: inset 0 -1em 1em rgba(255, 255, 255, 0.1),
+			0 0 0 1px rgba(255, 255, 255, 0.1), 0.3em 0.2em 3em rgba(0, 0, 0, 0.2);
+	}
 	.display {
 		display: flex;
 		flex-direction: row;
@@ -28,5 +45,6 @@
 		justify-content: center;
 		align-content: center;
 		gap: 25px;
+		background-color: black;
 	}
 </style>
