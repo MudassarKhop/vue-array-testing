27ae27e873089325843a9fb06309da40d936344a
MudassarKhop <khopatkarmudassar@gmail.com>
2022-07-12 10:04:06 +0200

    pictures

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="about">
+		<div class="card-display py-4"></div>
+	</div>
+</template>
---
+++ src/views/CardView.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<card v-for="product in products" :key="product.id" :product="product" />
+	</div>
+</template>
+<script>
+	import Card from "@/components/Card.vue";
+	export default {
+		components: { Card },
+		data() {
+			return {
+				products: [],
+			};
+		},
+		mounted() {
+			fetch(" http://localhost:3000/products")
+				.then((res) => res.json())
+				.then((data) => (this.products = data))
+				.catch((err) => console.log(err.message));
+		},
+	};
+</script>
+<style></style>
